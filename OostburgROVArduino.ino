4ac84864e33220692f32b16e0e65df8ab3d1c9e4
irondeau <isaiahrondeau@gmail.com>
2018-02-07 14:53:12 -0600

    Add files via upload

--- OostburgROVArduino.ino
+++ OostburgROVArduino.ino
@@ -1,25 +1,39 @@
 int serialVal = 30;
 int motorCtrlSpd;
+int directionalControl = 0;
 
 int motor1Direction;
 int motor2Direction;
 int motor3Direction;
 int motor4Direction;
+int motor5Direction;
+int motor6Direction;
 
 int motor1PWMVal;
 int motor2PWMVal;
 int motor3PWMVal;
 int motor4PWMVal;
+int motor5PWMVal;
+int motor6PWMVal;
 
-const int motor1DIR = 22;
-const int motor2DIR = 24;
-const int motor3DIR = 26;
-const int motor4DIR = 28;
+int motor1DIR = 22;
+int motor2DIR = 24;
+int motor3DIR = 26;
+int motor4DIR = 28;
+int motor5DIR = 30;
+int motor6DIR = 32;
 
-const int motor1PWM = 13;
-const int motor2PWM = 12;
-const int motor3PWM = 11;
-const int motor4PWM = 10;
+int motor1PWM = 13;
+int motor2PWM = 12;
+int motor3PWM = 11;
+int motor4PWM = 10;
+int motor5PWM = 9;
+int motor6PWM = 8;
+
+int dirBoolFB1 = LOW;
+int dirBoolFB2 = HIGH;
+int dirBoolLR1 = LOW;
+int dirBoolLR2 = HIGH;
 
 void setup() {
 
@@ -31,8 +45,12 @@ void setup() {
   pinMode(motor3PWM, OUTPUT);
   pinMode(motor4DIR, OUTPUT);
   pinMode(motor4PWM, OUTPUT);
+  pinMode(motor5DIR, OUTPUT);
+  pinMode(motor5PWM, OUTPUT);
+  pinMode(motor6DIR, OUTPUT);
+  pinMode(motor6PWM, OUTPUT);
 
- Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -45,7 +63,7 @@ void loop() {
   //LEFT JOYSTICK
   if(serialVal <= 28 && serialVal >= 0) {               //Between values 0 and 28
     
-    motor1Direction = LOW;
+    motor1Direction = dirBoolFB1;
     digitalWrite(motor1DIR, motor1Direction);
     motor1PWMVal = abs((serialVal - 32) * motorCtrlSpd);
     analogWrite(motor1PWM, motor1PWMVal);
@@ -56,7 +74,7 @@ void loop() {
     
   } else if(serialVal <= 64) {                          //Between values 33 and 64
     
-    motor1Direction = HIGH;
+    motor1Direction = dirBoolFB2;
     digitalWrite(motor1DIR, motor1Direction);
     motor1PWMVal = (serialVal - 32) * motorCtrlSpd;
     analogWrite(motor1PWM, motor1PWMVal);
@@ -64,7 +82,7 @@ void loop() {
     //RIGHT JOYSTICK
   } else if(serialVal <= 94) {                          //Between values 34 and 94
 
-    motor2Direction = LOW;
+    motor2Direction = dirBoolFB1;
     digitalWrite(motor2DIR, motor2Direction);
     motor2PWMVal = abs((serialVal - 97) * motorCtrlSpd);
     analogWrite(motor2PWM, motor2PWMVal);
@@ -75,7 +93,7 @@ void loop() {
   
   } else if(serialVal <= 129) {                         //Between values 99 and 129
 
-    motor2Direction = HIGH;
+    motor2Direction = dirBoolFB2;
     digitalWrite(motor2DIR, motor2Direction);
     motor2PWMVal = (serialVal - 97) * motorCtrlSpd;
     analogWrite(motor2PWM, motor2PWMVal);
@@ -102,21 +120,141 @@ void loop() {
     //BUMPERS
   } else if(serialVal == 195) {
 
-    motor4Direction = LOW;
+    motor4Direction = dirBoolLR1;
     digitalWrite(motor4DIR, motor4Direction);
-    motor4PWMVal = 150;
+    motor4PWMVal = 175;
     analogWrite(motor4PWM, motor4PWMVal);
+
+    motor5Direction = dirBoolLR1;
+    digitalWrite(motor5DIR, motor5Direction);
+    motor5PWMVal = 175;
+    analogWrite(motor5PWM, motor5PWMVal);
     
   } else if(serialVal == 196) {
 
-    motor4Direction = HIGH;
+    motor4Direction = dirBoolLR2;
     digitalWrite(motor4DIR, motor4Direction);
-    motor4PWMVal = 150;
+    motor4PWMVal = 175;
     analogWrite(motor4PWM, motor4PWMVal);
+
+    motor5Direction = dirBoolLR2;
+    digitalWrite(motor5DIR, motor5Direction);
+    motor5PWMVal = 175;
+    analogWrite(motor5PWM, motor5PWMVal);
     
   } else if(serialVal == 197) {
 
     analogWrite(motor4PWM, 0);
+    analogWrite(motor5PWM, 0);
+
+    //GRIPPER
+  } else if(serialVal == 198) {
+
+    motor6Direction = LOW;
+    digitalWrite(motor6DIR, motor6Direction);
+    motor6PWMVal = 150;
+    analogWrite(motor6PWM, motor6PWMVal);
     
+  } else if(serialVal == 199) {
+    
+    motor6Direction = HIGH;
+    digitalWrite(motor6DIR, motor6Direction);
+    motor6PWMVal = 150;
+    analogWrite(motor6PWM, motor6PWMVal);
+    
+  } else if(serialVal == 200) {
+
+    analogWrite(motor6Direction, 0);
+
+    //Multidirectional Control
+  } else if(serialVal >= 205 && serialVal <= 208) {
+
+    switch(serialVal) {
+      
+      //NORTH
+      case 205:
+      
+        motor1DIR = 22;
+        motor2DIR = 24;
+        motor4DIR = 28;
+        motor5DIR = 30;
+        
+        motor1PWM = 13;
+        motor2PWM = 12;
+        motor4PWM = 10;
+        motor5PWM = 9;
+
+        dirBoolFB1 = LOW;
+        dirBoolFB2 = HIGH;
+        dirBoolLR1 = HIGH;
+        dirBoolLR2 = LOW;
+        
+        break;
+        
+      //EAST
+      case 206:
+      
+        motor1DIR = 30;
+        motor2DIR = 28;
+        motor4DIR = 22;
+        motor5DIR = 24;
+        
+        motor1PWM = 9;
+        motor2PWM = 10;
+        motor4PWM = 13;
+        motor5PWM = 12;
+
+        dirBoolFB1 = HIGH;
+        dirBoolFB2 = LOW;
+        dirBoolLR1 = HIGH;
+        dirBoolLR2 = LOW;
+        
+        break;
+
+      //SOUTH
+      case 207:
+      
+        motor1DIR = 24;
+        motor2DIR = 22;
+        motor4DIR = 30;
+        motor5DIR = 28;
+        
+        motor1PWM = 12;
+        motor2PWM = 13;
+        motor4PWM = 9;
+        motor5PWM = 10;
+
+        dirBoolFB1 = HIGH;
+        dirBoolFB2 = LOW;
+        dirBoolLR1 = LOW;
+        dirBoolLR2 = HIGH;
+        
+        break;
+
+      //WEST
+      case 208:
+      
+        motor1DIR = 28;
+        motor2DIR = 30;
+        motor4DIR = 24;
+        motor5DIR = 22;
+        
+        motor1PWM = 10;
+        motor2PWM = 9;
+        motor4PWM = 12;
+        motor5PWM = 13;
+
+        dirBoolFB1 = LOW;
+        dirBoolFB2 = HIGH;
+        dirBoolLR1 = LOW;
+        dirBoolLR2 = HIGH;
+        
+        break;
+
+      default:
+      
+        break;
+      
+    }
   }
 }
